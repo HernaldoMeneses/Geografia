9fe9176979ddb77e797c20192908cc31276410ea
HernaldoMeneses <hernaldomeneses@gmail.com>
2024-03-03 17:38:48 -0300

    1.1-Espaco-Geográfico.py

---
+++ 1.1-Espaco-Geografico.py
@@ -0,0 +1,7 @@
+espaco_geogragico = [
+    'transformação da natureza',
+    'objetivando garantir subsistência do grupo social e melhorar condições de vida',
+    'artificialização da natureza'
+    'Tecnologia',
+    'principalmente após a Revolução Industrial (século XVIII)'
+    ]
