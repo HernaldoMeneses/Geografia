15a0ce5344adf643fd12bdf84369566af26302fc
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-07 17:12:33 -0300

    1.1_spaco-geografico

--- 1.1-Espaco-Geografico.py
+++ 1.1-Espaco-Geografico.py
@@ -5,3 +5,20 @@ espaco_geogragico = [
     'Tecnologia',
     'principalmente após a Revolução Industrial (século XVIII)'
     ]
+
+meio-tecno-cientifico-informacional = [
+    'Capitalismo',
+    'Ciência',
+    'técnica',
+    'Final do século XIX',
+    'Segunda Revolução Industrial',
+    'Mobilização intença da ciência visando novas tecnicas de produção e circulação em massa',
+    'Incoporação das tecnicas ao território',
+    'De natural, as sociedades avançaram para um meio cada vez mais técnico',
+    'Principalmente a partir da primeira Revolução Industrial (Sec XVIII)',
+
+    ''
+]
+
+#10/364
+#file:///L:/Linear_/Base-Jump_07/Geografia%20-%20Geral%20e%20do%20Brasil%20volume%201%20-%20Jo%C3%A3o%20Carlos%20Moreira%20e%20Eust%C3%A1quio%20de%20Sene.pdf
